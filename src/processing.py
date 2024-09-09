03501f79d6803fc2f9a84c09a8d7805497d06842
Ivan Bessonov <bessonov.ivan@gmail.com>
2024-09-09 22:30:21 +0500

    создан файл сохдание функцииfilter_by_state

--- src/processing.py
+++ src/processing.py
@@ -1,5 +1,12 @@
-def filter_by_state():
-    pass
+from typing import List, Dict, Any
 
-def sort_by_date():
-    pass
+
+def filter_by_state(data: List[Dict[str, Any]], state: str = 'EXECUTED') -> List[Dict[str, Any]]:
+    """Функция возвращает новый список словарей, у которых ключ state=EXECUTED"""
+    return [d for d in data if d.get('state') == state]
+
+
+def sort_by_date(date_list: list, reverse_list: bool = True) -> list | bool:
+    """Функция возвращает новый список, отсортированный по дате"""
+    sorted_list = sorted(date_list, key=lambda date_dict: date_dict.get("date"), reverse=reverse_list)
+    return sorted_list
