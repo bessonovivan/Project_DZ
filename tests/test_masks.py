45b7ef17c16f79c6d25a02006a537a316a2dbf7d
Ivan Bessonov <bessonov.ivan@gmail.com>
2025-01-21 23:17:54 +0500

    test for masks

---
+++ tests/test_masks.py
@@ -0,0 +1,10 @@
+from src.masks import get_mask_card_number, get_mask_account
+
+
+def test_get_mask_card_number():
+    assert get_mask_card_number('7000792289606361') == '7000 79** **** 6361'
+    assert get_mask_card_number('7000555289606059') == '7000 55** **** 6059'
+
+
+def test_get_mask_account():
+    assert get_mask_account('73654108430135874305') == '**4305'
